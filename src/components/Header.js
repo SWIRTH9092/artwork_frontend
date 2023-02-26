a3bba83a371343389e397746c97dad9e095170ba
Your Name <you@example.com>
2023-02-26 13:57:33 -0500

    add Header and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,43 @@
-const Header = (props) => {
-    return <h1>Header</h1>;
-  };
+//----------------------
+//  Import Dependencies
+//----------------------
+
+import { Link } from "react-router-dom"
+
+//----------------------
+//  Header
+//----------------------
+
+
+function Header(props) {
   
-  export default Header;
+    //Sytling
+    const headerContainer = {
+        textAlign: "center",
+        backgroundColor: "#0B2027",
+        color: "#F6F1D1",
+        height: "8rem"
+     }
   
+     const navBar = {
+        textDecoration: "none",
+        color: "#F6F1D1"
+     }
+  
+    return (
+    <div style={headerContainer}>
+        <br></br>
+        <h1>Grandma's Art Collection</h1>
+        <nav>
+            <Link to="/" style={navBar}>
+               <div>Home</div>
+            </Link>
+        </nav>
+    </div>
+  )
+}
+//----------------------
+//  Export header
+//----------------------
+
+export default Header
