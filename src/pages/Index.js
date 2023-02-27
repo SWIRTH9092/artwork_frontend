91783b4d19a090913ecb17bfb0d63499bb7026a6
Your Name <you@example.com>
2023-02-26 21:57:58 -0500

    styling for index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -22,6 +22,15 @@ function Index (props) {
 
 
     //Sytling
+
+    const addContainer = {
+      display: "flexbox",
+      justifyContent: "center",
+      textAlign: "center",
+      backgroundColor: "#F6F1D1",
+      color: "#0B2027"
+    }
+
     const artworkContainer = {
       display: "grid",
       gridTemplateColumns: "repeat(auto-fit, minmax(14rem, max-content))",
@@ -41,7 +50,7 @@ function Index (props) {
     }
   
     const imageSize = {
-      maxWidth: "200px"
+      width: "200px"
     }
 
     const createForm = {
@@ -56,68 +65,77 @@ function Index (props) {
 
     const createFormButton = {
       fontSize: "1.40rem",
-      color: "white",
       width: "23rem",
-      background: "rgb(25, 25, 25)",
+      background: "#0B2027",
+      color: "#F6F1D1",
       borderRadius: "10px"
     }
     
     return (
         <div>
-            <h2 style={centerTitle}>Add Artwork</h2>
-            <Form style={formCenter}
-              onSubmit={(event) => { 
-                setSubject('')
-                setCategory('')
-                setComments('')
-                setImage_url('')
-              }}
-              action="/create" method="post">
+            <div style={addContainer}>
+              <br></br>
+              <h2 style={centerTitle}>Add to the Collection</h2>
+              <Form style={formCenter}
+                onSubmit={(event) => { 
+                  setSubject('')
+                  setCategory('')
+                  setComments('')
+                  setImage_url('')
+                }}
+                action="/create" method="post">
 
-                <input type="text" name="subject" placeholder="subject" 
-                  value={subject} required style={createForm}
-                  onChange={e => {
-                    setSubject(e.target.value)
-                  }}
-                />     
+                  <input type="text" name="subject" placeholder="subject" 
+                    value={subject} required style={createForm}
+                    onChange={e => {
+                      setSubject(e.target.value)
+                    }}
+                  />     
 
-                <input type="text" name="category" placeholder="category" 
-                  value={category} required style={createForm}
-                  onChange={e => {
-                    setCategory(e.target.value)
-                  }}
-                />
+                  <input type="text" name="category" placeholder="category" 
+                    value={category} required style={createForm}
+                    onChange={e => {
+                      setCategory(e.target.value)
+                    }}
+                  />
+                  <br></br>
+                  <input type="text" name="comments" placeholder="comments" 
+                    value={comments} required style={createForm}
+                    onChange={e => {
+                      setComments(e.target.value)
+                    }}
+                  />
 
-                <input type="text" name="comments" placeholder="comments" 
-                  value={comments} required style={createForm}
-                  onChange={e => {
-                    setComments(e.target.value)
-                  }}
-                />
-
-                <input type="text" name="image_url" placeholder="image" 
-                  value={image_url} required style={createForm}
-                  onChange={e => {
-                    setImage_url(e.target.value)
-                  }}
-                />
-                <input type="submit" value="Add Artwork"
-                  style={createFormButton} />
-            </Form>
+                  <input type="text" name="image_url" placeholder="image" 
+                    value={image_url} required style={createForm}
+                    onChange={e => {
+                      setImage_url(e.target.value)
+                    }}
+                  />
+                  <br></br>
+                  <br></br>
+                  <input type="submit" value="Add Artwork"
+                    style={createFormButton} />
+              </Form>
+              <br></br>
+              <br></br>
+            </div>
+            <div>
             <br></br>
             <br></br>
                 <div style={artworkContainer}>
                   {artworks.map(artwork => (
                     <div key={artwork.id} style={artworkCard}>
+                        <img src={artwork.image_url} alt={artwork.subject} style={imageSize}/>
                         <Link to={`/${artwork.id}`}>
                             <h2>{artwork.subject}</h2>
                         </Link>
                         <h3>Category: {artwork.category}</h3>
-                        <img src={artwork.image_url} alt={artwork.subject} style={imageSize}/>
                         <br></br>
                     </div>
                   ))}
                 </div>
+              </div>
         </div>
     )
 }
